85e4be575a10f97a5c4856046f44d9f2f6e4b7b6
囧囧JOJO <41473909@qq.com>
2021-03-21 17:44:38 +0800

    auto registration account

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os,requests,json
+import os,requests,json,re
+from temporary_email import TemporaryEmail
 
 def push(key,title,content):
 
@@ -34,21 +35,103 @@ def getpro(token):
     response = requests.post('http://fakelocation.api.lerist.cc:8000/FakeLocation/user/trypro', json=data_json,headers=headers)
     return response.text
 
+def checkUserExist(user,type,):
+    headers = {
+        'User-Agent': 'okhttp/4.2.2',
+        'Connection': 'Keep-Alive',
+        'Accept-Encoding': 'gzip',
+        'Content-Type': 'application/json; charset=UTF-8'
+    }
+    data_json = {
+    "loginName": user,
+    "loginType": type,
+    "deviceId": "547f6feb12d6411d",
+    "deviceModel": "OnePlus HD1900",
+    "deviceSdkCode": 29,
+    "deviceSdkName": "10",
+    "flavor": "CN",
+    "language": "zh",
+    "packageName": "com.lerist.fakelocation",
+    "timezone": "GMT+08:00",
+    "versionCode": "906",
+    "versionName": "1.2.1.8"
+    }
+    response = requests.post('http://fakelocation.api.lerist.cc:8000/FakeLocation/user/checkUserExist', json=data_json,
+                             headers=headers)
+    return response.text
 
-user = os.getenv("user")
-password = os.getenv("password")
-type = os.getenv("type")
-if(type == ''):
-    type = 'email'
+def sendEmailCode(user):
+    headers = {
+        'User-Agent': 'okhttp/4.2.2',
+        'Connection': 'Keep-Alive',
+        'Accept-Encoding': 'gzip',
+        'Content-Type': 'application/json; charset=UTF-8'
+    }
+    data_json = {
+        "deviceId": "547f6feb12d6411d",
+        "emailAddress": user,
+        "timezone": "GMT+08:00",
+        "type": "login"
+    }
+    response = requests.post('http://fakelocation.api.lerist.cc:8000/FakeLocation/user/requestEmailVercode', json=data_json,
+                             headers=headers)
+    return response.text
+
+def regAccount(user,code):
+    headers = {
+    'User-Agent': 'okhttp/4.2.2',
+    'Connection': 'Keep-Alive',
+    'Accept-Encoding': 'gzip',
+    'Content-Type': 'application/json; charset=UTF-8'
+    }
+    data_json ={
+    "loginName": user,
+    "loginType": "email",
+    "vercode": code,
+    "deviceId": "547f6feb12d6411d",
+    "deviceModel": "OnePlus HD1900",
+    "deviceSdkCode": 29,
+    "deviceSdkName": "10",
+    "flavor": "CN",
+    "language": "zh",
+    "packageName": "com.lerist.fakelocation",
+    "timezone": "GMT+08:00",
+    "versionCode": "906",
+    "versionName": "1.2.1.8"
+}
+    response = requests.post('http://fakelocation.api.lerist.cc:8000/FakeLocation/user/login',json=data_json,headers=headers)
+    return response.text
+
+
+
+password = '123456789'
+type = 'email'
 send_key = os.getenv("SEND")
-login_info = login(user,password,type)
-login_code = json.loads(login_info).get('code')
-if(login_code == 200):
-    token = json.loads(login_info).get('body')['token']
+
+while True:
+    email = TemporaryEmail()
+    user = email.get_email_address()
+    if('用户不存在' in checkUserExist(user,type)):
+        break
+sendEmailCode(user)
+while True:
+    if(email.check_received_email()):
+        break
+email_content = email.get_email_content()
+
+pattern = '<a href="#">(.*)</a></span>'
+emailCode = (re.findall(pattern,email_content[0])[0])
+reg_info = regAccount(user,emailCode)
+if(json.loads(reg_info).get('success')):
+    token = json.loads(reg_info).get('body')['token']
     getpro_info = getpro(token)
     getpro_code = json.loads(getpro_info).get('code')
     if(getpro_code == 200):
         print(json.loads(getpro_info).get('message'))
+        push(send_key, 'FakeLocation 账号', user+'<br>'+getpro_info)
     else:
         print(getpro_info)
         push(send_key, 'FakeLocation 失败', getpro_info)
+else:
+    print(reg_info)
+    push(send_key, 'FakeLocation 失败', reg_info)
